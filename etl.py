64da06d0d0b9eeb6300e35632e2d6cf3ebc15aa2
şeref can memiş <canmemis@marun.edu.tr>
2025-07-18 14:40:42 +0300

    ETL improvement

--- etl.py
+++ etl.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
-import mysql.connector
+import seaborn as sns
+import matplotlib.pyplot as plt
 import os
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
-import seaborn as sns
-import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
 
+# load .env
 load_dotenv(r"C:\Users\şerefcanmemiş\Documents\projects_2\data_handle\.env")
 
 user = os.getenv('user')
@@ -15,11 +16,13 @@ host = os.getenv('host')
 database = os.getenv('database')
 folder = os.getenv('folder')
 
+# start the engine
 engine = create_engine(
     f'mysql+pymysql://{user}:{password}@{host}:3306/{database}?charset=utf8mb4',
     connect_args={"local_infile": 1}
 )
 
+# define tables
 tables = [
     'application_combined',
     'bureau',
@@ -32,16 +35,18 @@ tables = [
 ]
 dfs = {}
 
+# Extract tables from mysql to python dictionaries
 for table in tables:
     dfs[table] = pd.read_sql(f"select * from {table}",con = engine)
     print(f'{table} successfully added into the dfs')
     
 print(dfs.keys())
+
+# divide into train and test
 dfs['application_train'] = dfs['application_combined'][dfs['application_combined']['TARGET'].notna()].copy()
 dfs['application_test'] = dfs['application_combined'][dfs['application_combined']['TARGET'].isna()].copy()
-
 dfs.pop('application_combined')
-print(dfs.keys())
+
 
 # Checking null values
 tables = [
@@ -55,6 +60,8 @@ tables = [
     'previous_application',
     'sample_submission'
 ]
+
+# Check the null percentage of columns
 def null_percentage():
     for table in tables:
         namesofnull = dfs[table].isna().sum()[dfs[table].isna().sum() > 0].sort_values(ascending = False).index
@@ -67,6 +74,7 @@ def null_percentage():
         'null_percentage': percentage
     })
         print(df_nulls[df_nulls['null_percentage'] > 0])
+null_percentage()
     
 # We have null values only in application_train and application_test, others don't have NA values
 
@@ -84,16 +92,76 @@ def drop_null_colums(percentage):
         else:
             print(f'*{table}* no column to drop')
             
-null_percentage()
 
+# Copying our train and test tables
 app_train = dfs['application_train'].copy()
 app_test = dfs['application_test'].copy()
+app_train = app_train[app_test.columns]
+
+# Check if the 2 tables columns are the same
+if list(app_train.columns) == list(app_test.columns):
+    print('test and train columns are equal')
+else:
+    print('test and train columns are not equal!')
+    
+# Check if the values in column are same in both of the tables (bc they are going to be problem in onehotencoding)
+app_test['CODE_GENDER'].value_counts()
+app_train['CODE_GENDER'].value_counts() # just 4 XNA lets drop them
+app_train = app_train[app_train['CODE_GENDER'] != 'XNA']
+
+app_train['NAME_FAMILY_STATUS'].value_counts() # just 2 unkown lets also drop them
+app_test['NAME_FAMILY_STATUS'].value_counts()
+app_train = app_train[app_train['NAME_FAMILY_STATUS'] != 'Unknown']
+
+app_train['NAME_INCOME_TYPE'].value_counts() # 5 maternity leave
+app_test['NAME_INCOME_TYPE'].value_counts()
 
 
+# Check class imbalance
 app_train['TARGET'].value_counts()
 
+# Check data types counts
 app_train.dtypes.value_counts()
 
-app_train.select_dtypes('object').apply(pd.Series.nunique)
+# Check unique count in categorical data
+app_train.select_dtypes('object').apply(lambda col: col.nunique()).sort_values(ascending = False)
 
 # if object nunique > 2 onehotencoder else labelencoder
+print(app_train.select_dtypes('object'))
+
+def labelencode_df():
+    le = LabelEncoder()
+    for col in app_train.select_dtypes('object'):
+        if app_train[col].nunique() <= 2:
+            le.fit(app_train[col])
+            app_train[col] = le.transform(app_train[col])
+            
+            if set(app_test[col].dropna().unique()).issubset(le.classes_):
+                app_test[col] = le.transform(app_test[col])
+            else:
+                print(f'{col} has unseen labels in test skipping encoding')
+
+
+def onehotencode_df(df):
+    encoded_df = pd.get_dummies(df, dtype='int')
+    return encoded_df
+
+labelencode_df()
+app_train = onehotencode_df(df = app_train)
+app_test = onehotencode_df(df = app_test)
+
+print(app_train.shape) # has 1 extra column we need to figure out how to equalize columns
+print(app_test.shape)
+set(app_train) - set(app_test) # 'NAME_INCOME_TYPE_Maternity leave'
+set(app_test) - set(app_train)
+
+idx_NITM = app_train.columns.get_loc('NAME_INCOME_TYPE_Maternity leave')
+
+if 'NAME_INCOME_TYPE_Maternity leave' not in app_test.columns:
+    app_test.insert(idx_NITM, 'NAME_INCOME_TYPE_Maternity leave', pd.Series([0*len(app_test)]))
+
+set(app_train) - set(app_test)
+set(app_test) - set(app_train)
+
+# Columns inconsistency problem solved
+

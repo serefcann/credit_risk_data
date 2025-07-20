f9dd0348a47f0e4fb5547e5e6cb747887328448f
şeref can memiş <canmemis@marun.edu.tr>
2025-07-20 19:56:14 +0300

    continue with modelling

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 import os
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, PolynomialFeatures
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LogisticRegression
 
 # load .env
 load_dotenv(r"C:\Users\şerefcanmemiş\Documents\projects_2\data_handle\.env")
@@ -180,13 +182,127 @@ corr = app_train.corr()['TARGET'].sort_values(ascending=True)
 corr.tail(30)
 corr.head(30)
 
-app_train['DAYS_BIRTH_YEARS'] = -app_train['DAYS_BIRTH'] / 365
+# There are some mistakes about days employed bc 365243 can not be real employed days
+app_train['DAYS_EMPLOYED'].describe()
+app_train['DAYS_EMPLOYED'].value_counts()
+plt.hist(app_train['DAYS_EMPLOYED'])
+plt.show()
+
+# lets handle them
+anom = app_train[app_train['DAYS_EMPLOYED'] == 365243]
+non_anom = app_train[app_train['DAYS_EMPLOYED'] != 365243]
+
+print("anomalies default", anom['TARGET'].mean() * 100, "%")
+print("non nomalies default", non_anom['TARGET'].mean() * 100, "%")
+print(f"There are {len(anom)} anom days of employment in train set")
+
+# Creating employed days anomaly flag
+app_train['DAYS_EMPLOYED_ANOM'] = app_train['DAYS_EMPLOYED'] == 365243
+# anomalies returning to NA values
+app_train['DAYS_EMPLOYED'].replace(365243,np.nan, inplace= True)
+
+app_test['DAYS_EMPLOYED_ANOM'] = app_test['DAYS_EMPLOYED'] == 365243
+# anomalies returning to NA values
+app_test['DAYS_EMPLOYED'].replace(365243,np.nan, inplace= True)
+print('There are %d anom in the test data out of %d entries' % (app_test["DAYS_EMPLOYED_ANOM"].sum(), len(app_test)))
+
 
 # Checking whether younger or older people are linked to late repayment of the loan
-agg = app_train[['DAYS_BIRTH_YEARS','DAYS_BIRTH','TARGET','AMT_CREDIT']].copy()
-agg['DAYS_BIRTH_YEARS'] = pd.qcut(agg['DAYS_BIRTH_YEARS'], q= 8)
-agg.groupby('DAYS_BIRTH_YEARS').mean()
+agg = app_train[['DAYS_BIRTH','TARGET','AMT_CREDIT']].copy()
+agg['DAYS_BIRTH_YEARS'] = -app_train['DAYS_BIRTH'] / 365
+
+# showing skewness of target 0 and 1, younger people more tend to target 1
+sns.kdeplot(agg.loc[agg['TARGET'] == 1]['DAYS_BIRTH_YEARS'], color='red')
+sns.kdeplot(agg.loc[agg['TARGET'] == 0]['DAYS_BIRTH_YEARS'], color='blue')
+plt.show()
+
+agg['DAYS_BIRTH_YEARS'] = pd.qcut(agg['DAYS_BIRTH_YEARS'], q = 8)
+age_groups = agg.groupby('DAYS_BIRTH_YEARS').mean()
+age_groups
+
+# the higher the age, the closer the target gets to 0 (which mean can repay the loan in time)
+plt.bar(x=age_groups.index.astype(str), height=age_groups['TARGET'].to_numpy())
+plt.show()
+
+ext_table = app_train[['EXT_SOURCE_1','EXT_SOURCE_2','EXT_SOURCE_3','TARGET','DAYS_BIRTH']]
+# correlation table
+ext_corr = ext_table.corr()
+sns.heatmap(ext_corr, annot=True)
+plt.show()
+
+# lets look at ext1, ext2, ext2
+for i, ext in enumerate(['EXT_SOURCE_1','EXT_SOURCE_2','EXT_SOURCE_3']):
+    plt.subplot(3, 1, i + 1)
+    sns.kdeplot(ext_table.loc[ext_table['TARGET'] == 1][ext], color='red')
+    sns.kdeplot(ext_table.loc[ext_table['TARGET'] == 0][ext], color='blue')
+    plt.title(f'distribution of {ext} by Target')
+plt.show()
+
+app_test = app_test.drop('TARGET', axis = 1)
+
+poly_features_train = app_train[['EXT_SOURCE_1','EXT_SOURCE_2','EXT_SOURCE_3','DAYS_BIRTH','TARGET']].copy()
+poly_features_test =  app_test[['EXT_SOURCE_1','EXT_SOURCE_2','EXT_SOURCE_3','DAYS_BIRTH']].copy()
 
 
+poly_target = poly_features_train['TARGET'].copy()
+poly_features_train = poly_features_train.drop('TARGET',axis=1)
+# imputing na values
+imp = SimpleImputer(missing_values=np.nan, strategy='median')
+imp.fit(poly_features_train)
+poly_train = imp.transform(poly_features_train)
+poly_test = imp.transform(poly_features_test)
+
+# create Polynomial features
+poly_transformer = PolynomialFeatures(degree=3)
+poly_train = poly_transformer.fit_transform(poly_train)
+poly_test = poly_transformer.transform(poly_test)
+poly_columns = poly_transformer.get_feature_names_out(['EXT_SOURCE_1','EXT_SOURCE_2','EXT_SOURCE_3','DAYS_BIRTH'])
+
+poly_features_train = pd.DataFrame(data = poly_train, columns= poly_columns)
+poly_features_test = pd.DataFrame(data = poly_test, columns= poly_columns)
+print(poly_features_train.shape)
+print(poly_features_test.shape)
+
+poly_features_train['TARGET'] = poly_target
+
+# lets check out new features correlations
+poly_corr = poly_features_train.corr()['TARGET'].sort_values()
+poly_corr
+
+# Left join poly and app data
+poly_features_train['SK_ID_CURR'] = app_train['SK_ID_CURR']
+poly_features_test['SK_ID_CURR'] = app_train['SK_ID_CURR']
+app_train_poly = app_train.merge(poly_features_train, on='SK_ID_CURR', how= 'left')
+app_test_poly = app_test.merge(poly_features_test, on='SK_ID_CURR', how= 'left')
+print("Poly train shape:",app_train_poly.shape)
+print("Poly test shape", app_test_poly.shape)
+
+app_train_poly, app_test_poly = app_train_poly.align(app_test_poly, join='inner', axis = 1)
+print("Poly train shape:",app_train_poly.shape)
+print("Poly test shape", app_test_poly.shape)
 
 
+# Domain features
+app_train.columns[:20]
+app_train_domain = app_train.copy()
+app_test_domain = app_test.copy()
+
+app_train_domain['CREDIT_INCOME_PERCENT'] = app_train_domain['AMT_CREDIT'] / app_train_domain['AMT_INCOME_TOTAL']
+app_train_domain['DAYS_EMPLOYED_PERCENT'] = app_train_domain['DAYS_EMPLOYED'] / -app_train_domain['DAYS_BIRTH']
+app_train_domain['INCOME_PER_PERSON_FAMILY'] = app_train_domain['AMT_INCOME_TOTAL'] / app_train_domain['CNT_CHILDREN']
+app_train_domain['ANNUITY_INCOME_PERCENT'] = app_train_domain['AMT_ANNUITY'] / app_train_domain['AMT_INCOME_TOTAL']
+app_train_domain['CREDIT_TERM'] = app_train_domain['AMT_ANNUITY'] / app_train_domain['AMT_CREDIT']
+
+app_test_domain['CREDIT_INCOME_PERCENT'] = app_test_domain['AMT_CREDIT'] / app_test_domain['AMT_INCOME_TOTAL']
+app_test_domain['DAYS_EMPLOYED_PERCENT'] = app_test_domain['DAYS_EMPLOYED'] / -app_test_domain['DAYS_BIRTH']
+app_test_domain['INCOME_PER_PERSON_FAMILY'] = app_test_domain['AMT_INCOME_TOTAL'] / app_test_domain['CNT_CHILDREN']
+app_test_domain['ANNUITY_INCOME_PERCENT'] = app_test_domain['AMT_ANNUITY'] / app_test_domain['AMT_INCOME_TOTAL']
+app_test_domain['CREDIT_TERM'] = app_test_domain['AMT_ANNUITY'] / app_test_domain['AMT_CREDIT']
+
+
+# Let continue with Modelling
+train = app_train.copy()
+test = app_test.copy()
+
+train_target = train['TARGET']
+train.drop('TARGET',axis = 1, inplace= True)
